b9404597ab70133a89379f25ae86f305d45b1e6f
agustin.achagaperez <achagaperezagustin2015@gmail.com>
2024-11-11 15:29:58 -0300

    se agregaron componentes y se reorganizaron carpetas

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Car Agency</h1>
+      <h2>Rental & Purchase </h2>
+    </header>
+  );
+}
+
+export default Header;
