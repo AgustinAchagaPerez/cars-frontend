3c4f0f56bcb44cc06dc1d1b119e2a2feaf998828
agustin.achagaperez <achagaperezagustin2015@gmail.com>
2024-11-11 15:51:05 -0300

    se agregaron menú y footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <p>&copy; 2024 Cars Application <sup>®</sup> Todos los derechos reservados</p>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Menu = () => (
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Dashboard</li>
+      <li>Logout</li>
+    </ul>
+  </nav>
+);
+
+export default Menu;
