ba29d1fd67d0d61d99b62b4af6fe76326d4c80c2
agustin.achagaperez <achagaperezagustin2015@gmail.com>
2024-11-11 15:43:20 -0300

    se agregó logo al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 function Header() {
   return (
     <header>
+      <img src={cars_agency_logo} alt="Car Agency Logo" />
       <h1>Car Agency</h1>
-      <h2>Rental & Purchase </h2>
+      <h2>Rental & Purchase</h2>
     </header>
   );
 }
 
 export default Header;
+
